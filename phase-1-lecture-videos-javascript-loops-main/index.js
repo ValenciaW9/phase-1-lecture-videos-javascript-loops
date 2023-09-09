9dfdde811880ec4fc48f12eb1f432cf4e1f920eb
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-09 11:29:05 -0400

    phase-1-lecture-videos-javascript-loops/
    phase-1-lecture-videos-javascript-loops/

---
+++ phase-1-lecture-videos-javascript-loops-main/index.js
@@ -0,0 +1,107 @@
+//For
+
+let books = [' Eloquent  Javascript', 'Javascript : The Good Parts','Learn Javascript VISUALLY', 'You don\'t know js', 'Javascript: The Definitive Guide']
+
+let  onebook = {
+    title: 'Eloquent Javascript',
+    author: 'Narub biverbeke',
+    publisher: 'No Starch Press'
+}
+
+function loopThroughBooks(book)[
+    for()
+       //Statement 
+}
+
+function loopThroughBooks(books){
+    for(let i = 0; i < 2; i++){
+        //Statement
+        console.log(i < 2)
+        console.log(i)
+    }
+}
+loopThroughBooks(books)
+
+
+//Dynamic exmple
+
+let books = [' Eloquent  Javascript', 'Javascript : The Good Parts','Learn Javascript VISUALLY', 'You don\'t know js', 'Javascript: The Definitive Guide']
+
+let  onebook ={
+    title: 'Eloquent Javascript',
+    author: 'Narub biverbeke',
+    publisher: 'No Starch Press'
+}
+
+function loopThroughBooks(book)[
+    for()
+       //Statement 
+
+
+function loopThroughBooks(books){
+    for(let i = 0; i < books.length; i++){
+        //Statement
+        console.log(i < 2)
+        console.log(i)
+    }
+}
+loopThroughBooks(books)
+
+
+///
+
+let books = [' Eloquent  Javascript', 'Javascript : The Good Parts','Learn Javascript VISUALLY', 'You don\'t know js', 'Javascript: The Definitive Guide']
+
+let  onebook = {
+    title: 'Eloquent Javascript',
+    author: 'Narub biverbeke',
+    publisher: 'No Starch Press'
+}
+
+function loopThroughBooks(book)[
+    for()
+       //Statement 
+
+
+function loopThroughBooks(books){
+    for(let i = 0; i < books.length; i++){
+        //Statement
+        console.log(books[i])
+        
+    }
+}
+loopThroughBooks(books)
+
+
+
+///for... of examples
+
+
+let books = [' Eloquent  Javascript', 'Javascript : The Good Parts','Learn Javascript VISUALLY', 'You don\'t know js', 'Javascript: The Definitive Guide']
+
+let onebook = {
+    title: 'Eloquent Javascript',
+    author: 'Narub biverbeke',
+    publisher: 'No Starch Press'
+}
+
+function loopThroughBooks(book)[
+    
+       //Statement  for..of
+
+
+function loopThroughBooks(books){
+    for(let book  ofbooks){
+        console.log(book)
+       }
+    }
+
+//loopThroughBooks(books)
+
+function loopThroughObj(obj){
+    for(let key in obj){
+        console,log(obj[key])
+    }
+}
+
+loopThroughObj(onebook)
